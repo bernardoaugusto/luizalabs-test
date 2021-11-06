62d94829b32e7b3e830f65156c4da2e2536f0e1a
Bernardo Augusto Bastos Silva <bernardo.augusto.bastos@gmail.com>
2021-11-06 13:03:00 -0300

    test: ensure return a customer by id

---
+++ src/modules/customers/services/getCustomerService.spec.ts
@@ -0,0 +1,33 @@
+import 'reflect-metadata';
+
+import GetCustomerService from './GetCustomerService';
+import { v4 as uuid } from 'uuid';
+
+describe('GetCustomerService', () => {
+    let getCustomerService: GetCustomerService;
+    let customersRepository: {
+        create: jest.Mock<any, any>;
+        findByEmail: jest.Mock<any, any>;
+        findById: jest.Mock<any, any>;
+    };
+
+    beforeEach(() => {
+        customersRepository = {
+            create: jest.fn(),
+            findByEmail: jest.fn(),
+            findById: jest.fn(),
+        };
+
+        getCustomerService = new GetCustomerService(customersRepository);
+    });
+
+    it('Should be return a customer by id', async () => {
+        const customerId = uuid();
+
+        customersRepository.findById.mockResolvedValue({ id: customerId });
+        const res = await getCustomerService.execute(customerId);
+
+        expect(res.id).toBe(customerId);
+        expect(customersRepository.findById).toHaveBeenCalledWith(customerId);
+    });
+});
