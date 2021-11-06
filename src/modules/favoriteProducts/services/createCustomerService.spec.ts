9df7aeda9fe7a9320249cb2b86695bbedc4b350b
Bernardo Augusto Bastos Silva <bernardo.augusto.bastos@gmail.com>
2021-11-06 10:44:38 -0300

    test: ensure return the created FavoriteProduct

---
+++ src/modules/favoriteProducts/services/createCustomerService.spec.ts
@@ -0,0 +1,42 @@
+import 'reflect-metadata';
+
+import AppError from '../../../shared/errors/AppError';
+import CreateFavoriteProductService from './CreateFavoriteProductService';
+import { v4 as uuid } from 'uuid';
+
+describe('CreateFavoriteProductService', () => {
+    let createFavoriteProductService: CreateFavoriteProductService;
+    let favoriteProductsRepository: {
+        create: jest.Mock<any, any>;
+    };
+
+    beforeEach(() => {
+        favoriteProductsRepository = {
+            create: jest.fn(),
+        };
+
+        createFavoriteProductService = new CreateFavoriteProductService(
+            favoriteProductsRepository,
+        );
+    });
+
+    it('Should return the created FavoriteProduct', async () => {
+        const sut = {
+            customerId: 'any_customerId',
+            productId: 'any_productId',
+        };
+
+        const favoriteProductId = uuid();
+        favoriteProductsRepository.create.mockResolvedValue({
+            ...sut,
+            id: favoriteProductId,
+        });
+        const res = await createFavoriteProductService.execute(sut);
+
+        expect(res).toEqual({
+            ...sut,
+            id: favoriteProductId,
+        });
+        expect(favoriteProductsRepository.create).toHaveBeenCalledWith(sut);
+    });
+});
