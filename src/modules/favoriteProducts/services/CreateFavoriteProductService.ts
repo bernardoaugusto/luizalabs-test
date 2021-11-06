32faddd19246354b1ddddcff580cfeb3ee6848c2
Bernardo Augusto Bastos Silva <bernardo.augusto.bastos@gmail.com>
2021-11-06 11:06:09 -0300

    feat: ensure return error if the product is already in the favorites list

--- src/modules/favoriteProducts/services/CreateFavoriteProductService.ts
+++ src/modules/favoriteProducts/services/CreateFavoriteProductService.ts
@@ -1,4 +1,5 @@
 import { injectable, inject } from 'tsyringe';
+import AppError from '../../../shared/errors/AppError';
 import ICreateFavoriteProductDTO from '../dtos/ICreateFavoriteProductDTO';
 import FavoriteProduct from '../infra/typeorm/entities/FavoriteProduct';
 import IFavoriteProductsRepository from '../repositories/IFavoriteProductsRepository';
@@ -14,6 +15,15 @@ export default class CreateFavoriteProductService {
         customerId,
         productId,
     }: ICreateFavoriteProductDTO): Promise<FavoriteProduct> {
+        const checProductIsAlreadyInFavorites =
+            await this.favoriteProductsRepository.findByCustomerIdAndProductId(
+                customerId,
+                productId,
+            );
+
+        if (checProductIsAlreadyInFavorites)
+            throw new AppError('The product is already in favorites');
+
         const favoriteProduct = new FavoriteProduct();
         Object.assign(favoriteProduct, {
             customerId,
