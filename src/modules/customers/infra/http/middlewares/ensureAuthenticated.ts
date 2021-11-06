6f3604629b6e03494728ea555a3535ad0755fa1b
Bernardo Augusto Bastos Silva <bernardo.augusto.bastos@gmail.com>
2021-11-06 08:50:18 -0300

    feat: add ensureAuthenticated

---
+++ src/modules/customers/infra/http/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,43 @@
+import { NextFunction, Request, Response } from 'express';
+
+import AppError from '../../../../../shared/errors/AppError';
+import authConfig from '../../../../../config/auth';
+import { verify } from 'jsonwebtoken';
+
+interface ITokenPayload {
+    iat: number;
+    exp: number;
+    sub: string;
+}
+
+export default function ensureAuthenticated(
+    request: Request & { user: { id: string } },
+    response: Response,
+    next: NextFunction,
+): void {
+    const authHeader = request.headers.authorization;
+
+    if (!authHeader) {
+        throw new AppError('JWT token is missing!', 401);
+    }
+
+    const [format, token] = authHeader.split(' ');
+
+    try {
+        if (format !== 'Bearer') {
+            throw new AppError('Invalid JWT token', 401);
+        }
+
+        const decodedToken = verify(token, authConfig.jwt.secret);
+
+        const { sub } = decodedToken as ITokenPayload;
+
+        request.user = {
+            id: sub,
+        };
+
+        return next();
+    } catch {
+        throw new AppError('Invalid JWT token', 401);
+    }
+}
